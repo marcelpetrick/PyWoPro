d26d28320813559716fdc92f09ce61f3fcbcacb9
Marcel Petrick <mpetrick@data-modul.com>
2023-05-31 14:59:28 +0200

    fix java problem with libreoffice

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def convert_to_pdf(docx_file, pdf_file):
     subprocess.run(command, shell=True, stdout=subprocess.DEVNULL)
 
     # rename the output file
-    default_pdf_file = docx_file.replace('.docx', '.pdf')
-    subprocess.run(f'mv {default_pdf_file} {pdf_file}', shell=True)
+    #default_pdf_file = docx_file.replace('.docx', '.pdf')
+    #subprocess.run(f'mv {default_pdf_file} {pdf_file}', shell=True)
 
 def split_pdf(manufacturer_spec, article_number):
     inputpdf = PdfFileReader(open("modified.pdf", "rb"))
@@ -74,3 +74,9 @@ if __name__ == "__main__":
     main()
 
 # python3 main.py --template=template_front_and_back.docx --articleNumber=3.1415 --logo=fancy_logo.png --manufacturerSpec=manufacturer_spec.pdf
+
+
+# javaldx: Could not find a Java Runtime Environment!
+# Warning: failed to read path from javaldx
+# Solved by:
+# sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
